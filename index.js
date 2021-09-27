c2af3a69014327148187f61bea0576cc25a96e3a
hernandw <hernandw@gmail.com>
2021-09-27 16:48:33 -0300

    creacion del servidor

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 http
-.createServer((req, res)=>{
-    res.writeHead(200, {'Content-type' : 'text/html'})
-    res.end(`<p>Desafio <b>Latam</b></p>`);
-}).listen(3000,()=>{
-    console.log('Servidor ON');
-})
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      res.end(data);
+    });
+  })
+  .listen(3000, () => {
+    console.log("Servidor ON");
+  });
