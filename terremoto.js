fb747f32008c77814c88ffadebcd292d67dca2fe
Williams Hernandez <hernandw@gmail.com>
2021-09-27 22:52:21 -0300

    ejercicio promesa

--- terremoto.js
+++ terremoto.js
@@ -1,20 +1,17 @@
 const axios = require('axios');
 const moment = require('moment');
-
-let fecha;
-
-let magnitud;
-
-const getData = async ()=>{
-let data = await axios.get('https://api.gael.cl/general/public/sismos');
-fecha = data[0].fecha;
-magnitud = data[0].magnitud;
+let nombre
+let nacimiento
+async function getData() {
+let { data } = await axios.get('https://randomuser.me/api/')
+let user = data.results[0]
+nombre = `${user.name.first} ${user.name.last}`
+nacimiento = moment(user.dob.date).format('Do MMMM YYYY');
 }
-
-const imprimir = () =>{
-    console.log(`El ultimo sismo fue ${fecha} y su magnitud fue de ${magnitud}`);
+function imprimir() {
+console.log(`El usuario ${nombre} nació el ${nacimiento}`)
 }
+getData().then(() => {
+imprimir()
+})
 
-getData().then(()=>{
-    imprimir()
-});
